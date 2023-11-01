bd79beaf0bc8eaff6a13ce9aab392b787a8f7c49
SageM <madduxsage@gmail.com>
2023-11-01 15:40:32 -0400

    busted drinkdetails page

--- cocktailParty/src/global.js
+++ cocktailParty/src/global.js
@@ -1,2 +1,3 @@
 export const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='
 export const RANDOM_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+export const ID_URL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
