242fd4e8a9d19a816582b22f4db0f79aa3759789
williamrogerschi <william.rogers.chi@gmail.com>
2023-10-31 19:05:36 -0500

    committing CSS changes to nav

--- cocktailParty/src/components/Home.jsx
+++ cocktailParty/src/components/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>I am an home</h1>
+            <div className="about-container">
+                <h2>About Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla a recusandae nisi, fuga quam ipsam minima quos praesentium corporis non. Vel quam, provident asperiores eveniet aperiam qui iste corporis! Sit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis cum vel dicta doloribus, obcaecati eligendi voluptates impedit, dolor praesentium nobis itaque quia modi quidem ipsam? Doloremque nesciunt saepe illo dignissimos.</p>
+            </div>
         </div>
     )
 }
